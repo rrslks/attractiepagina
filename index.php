6bca74f42283922d24881bd8112a5d3820f6c581
rrslks <arhire.rares07@gmail.com>
2025-05-07 11:20:12 +0200

    1

--- index.php
+++ index.php
@@ -19,14 +19,123 @@ require_once 'admin/backend/config.php';
 
 <body>
 
+<div class="filter-bar">
+    <select>
+      <option value="all">Alle attracties</option>
+      <option value="adventure">Avontuur</option>
+      <option value="family">Familie</option>
+      <option value="thrill">Sensatie</option>
+    </select>
+    <input type="text" placeholder="Zoek attracties...">
+  </div>
+
     <?php require_once 'header.php'; ?>
     <div class="container content">
-        <aside>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia modi dolore magnam! Iste libero voluptatum autem, sapiente ullam earum nostrum sed magnam vel laboriosam quibusdam, officia, esse vitae dignissimos nulla?
-        </aside>
+        <h2>Alle attracties!</h2>
         <main>
             <!-- hier komen de attractiekaartjes -->
         </main>
+    </div>
+
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Reuzenrad</h1>
+                <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                <p>Minimaale lengte: 120 cm</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/alex-kalinin-6gYjwD4s9xk-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Caroussel</h1>
+                <p>Voor kleine kindjes: Maak een rondje in de antieke draaimolen.</p>
+                <p>Minimaale lengte: cm</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/david-murcia-HbYniDwjbVE-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Thunderbolt</h1>
+                <p>Adrenaline, gegarandeerd.</p>
+                <p>Minimaale lengte: 140 cm lengte</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Reuzenrad</h1>
+                <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                <p>minimaal 120 cm lengte</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Reuzenrad</h1>
+                <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                <p>minimaal 120 cm lengte</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Reuzenrad</h1>
+                <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                <p>minimaal 120 cm lengte</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Reuzenrad</h1>
+                <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                <p>minimaal 120 cm lengte</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Reuzenrad</h1>
+                <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                <p>minimaal 120 cm lengte</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Reuzenrad</h1>
+                <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                <p>minimaal 120 cm lengte</p>
+            </div>
+        </div>
+    </div>
+    <div class="attracties">
+        <div class="attractie">
+            <div class="img" style="background-image: url('img/attracties/adger-kang-oiyzr-SgjBY-unsplash.jpg')"></div>
+            <div class="attractieinfo">
+                <h1>Reuzenrad</h1>
+                <p>Bekijk het park van grote hoogte met het hele gezin.</p>
+                <p>minimaal 120 cm lengte</p>
+            </div>
+        </div>
     </div>
 
 </body>
